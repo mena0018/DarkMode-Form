7661b9f6623873aa4beb5d76a2e363b4a03e501a
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-05-25 19:17:17 +0200

    Ajout du dark mode + de la logique

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Light mode
 
-![Capture d’écran 2022-05-25 à 15 45 28](https://user-images.githubusercontent.com/89834824/170277316-4e634c25-5b94-4e08-866a-2d972f18ba99.png)
+![Capture d’écran 2022-05-25 à 19 15 51](https://user-images.githubusercontent.com/89834824/170323273-d4a383d9-8ee9-4c32-b990-74c93b579112.png)
+
+
+# Dark mode
+
+![Capture d’écran 2022-05-25 à 19 14 58](https://user-images.githubusercontent.com/89834824/170323249-ed42d7d8-483d-49fd-ba7a-e342d9ac931d.png)
+
+
--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import Home from './Components/Home';
-import './index.scss';
+import Home from "./Components/Home";
+import "./index.scss";
+import { createContext, useState } from "react";
+import Switch from 'react-js-switch';
+
+
+export const ThemeContext = createContext(null);
 
 function App() {
-  return (
 
-    <div className="App">
-      <Home />
-    </div>
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => setTheme((theme) => theme === "light" ? "dark" : "light" );
+
+
+  return (
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      <div className="App" id={theme}>
+        <Home />
+        <Switch value={theme === "light" ? false : true} onChange={toggleTheme} />
+        <label htmlFor="">{theme === "light" ? "DarMode"  : "LightMode"}</label>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
